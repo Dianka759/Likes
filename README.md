25a5adc2b9e0c6ee8324e9d2b6bc7f05358e9160
dianka759 <dianka759@gmail.com>
2021-10-16 21:48:22 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Likes buttons
+
+Creating a sample news feed with like buttons, increasing the number of likes on click using JS. 
---
+++ script.js
@@ -0,0 +1,23 @@
+var count = 100;
+var countElement = document.querySelector("#count");
+
+function like() {
+    count++;
+    countElement.innerText = count + " likes"
+}
+
+var count2 = 99;
+var count2Element = document.querySelector("#count2");
+
+function like2() {
+    count2++;
+    count2Element.innerText = count2 + " likes"
+}
+
+var count3 = 101;
+var count3Element = document.querySelector("#count3");
+
+function like3() {
+    count3++;
+    count3Element.innerText = count3 + " likes"
+}
